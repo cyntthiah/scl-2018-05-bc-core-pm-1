8b00cd6c31e74aee0e1f28d3e545a41dea000dea
cynthia <cyntthiah@gmail.com>
2018-05-31 14:45:28 -0400

    cambios en clase

--- cipher.js
+++ cipher.js
@@ -26,5 +26,33 @@ window.cipher = {
   },
   decode: () => {
     /* Acá va tu código */
+    function DecodeCaesar(texto, numero)
+{
+    var cadena1 = texto.toUpperCase();
+    var cadena2 = "";
+    console.log(cadena1);
+    for(i=0; i<cadena1.length; i++)
+    {
+        var numero1 = cadena1.charCodeAt(i);
+        if(numero1 == 32)
+        {
+            cadena2 = cadena2 + String.fromCharCode(numero1);
+        }
+        else
+        {
+            var numero3 = numero1 - 65 - numero;
+            if (numero3<0)
+            {
+                numero3 = (26+numero3);    
+            }
+            numero2 = numero3%26+65;
+            console.log(numero1+ "-"+ numero2);
+            cadena2 = cadena2 + String.fromCharCode(numero2);
+        }
+       
+    }
+    console.log(cadena2);
+    return cadena2;
+}
   }
 }
